7d3a540c011fe4881aa90a70d867102809adc5a8
Tinuvile <3145593233@qq.com>
2025-07-06 00:25:10 +0800

    freestanding-rust-binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]  // 禁用标准库
+#![no_main]  // 禁用入口函数
+
+#[unsafe(no_mangle)]  // 禁用名称重整
+pub extern "C" fn _start() -> !{  // 使用C语言的调用约定
+    // 因为链接器会寻找一个名为_start的函数，所以这个函数就是入口点
+    // 默认命名为_start
+    loop{}
 }
+
+// 实现panic处理函数
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info:&PanicInfo)->!{
+    loop{}
+}
+
