3c5ef50346d081969b102bcdf1d859fc6942399e
Tinuvile <3145593233@qq.com>
2025-07-07 21:43:35 +0800

    section 01

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
 #![no_std]  // 禁用标准库
 #![no_main]  // 禁用入口函数
 
+#![feature(custom_test_frameworks)]
+#![test_runner(rustOS::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use rustOS::{println};
+
 #[unsafe(no_mangle)]  // 禁用名称重整
-pub extern "C" fn _start() -> !{  // 使用C语言的调用约定
+pub extern "C" fn _start() -> ! {  // 使用C语言的调用约定
     // 因为链接器会寻找一个名为_start的函数，所以这个函数就是入口点
     // 默认命名为_start
+
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop{}
 }
+
 
 // 实现panic处理函数
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]  // 在测试模式下不启用panic处理函数
 #[panic_handler]
-fn panic(_info:&PanicInfo)->!{
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
 
+#[cfg(test)]  // 在测试模式下用serial_println替代println
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rustOS::test_panic_handler(info)
+}
+
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
---
+++ src/vga_buffer.rs
@@ -0,0 +1,185 @@
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+
+#[allow(dead_code)]  // 禁用未使用的代码的警告
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]  // 启用复制语义，可打印且比较
+#[repr(u8)]  // 指定u8类型存储
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]  // 确保结构体字段的布局与C语言结构体一致
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+use volatile::Volatile;
+
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+
+pub struct Writer {
+    column_position: usize,  // 追踪最后一行的位置
+    color_code: ColorCode,  // 指定颜色
+    buffer: &'static mut Buffer,  // 指向VGA缓冲区的指针，使用'static生命周期，引用在整个程序运行期间有效
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),  // 换行
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {  // 如果列位置超出缓冲区宽度，则换行
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;  // 最后一行
+                let col = self.column_position;  // 当前列位置
+
+                let color_code = self.color_code;  // 获取颜色代码
+
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_str(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // 打印ASCII字符
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // 打印其他字符
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    /* 
+     * TODO:
+     * 目前的滚动实现方式效率较低，需要优化
+     * 可以考虑使用一个缓冲区来存储需要滚动的行，然后一次性将缓冲区中的行复制到屏幕上
+     * 或者维护一个虚拟起始行，每次滚动时，将虚拟起始行与实际起始行交换
+     */
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.column_position = 0;
+        self.clear_row(BUFFER_HEIGHT - 1);
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+
+use core::fmt;
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_str(s);
+        Ok(())
+    }
+}
+
+
+use spin::Mutex;
+use lazy_static::lazy_static;
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::LightGreen, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) }
+    });
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
+
+#[test_case]
+fn test_println_new_line() {
+    for _ in 0..200 {}
+}
