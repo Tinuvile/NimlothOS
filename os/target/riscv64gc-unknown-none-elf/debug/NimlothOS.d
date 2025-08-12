ac143e9425cba644c866c888c39015438196b58a
Tinuvile <3145593233@qq.com>
2025-08-12 15:42:22 +0800

    refactor: add github config and change folder position

---
+++ os/target/riscv64gc-unknown-none-elf/debug/NimlothOS.d
@@ -0,0 +1,1 @@
+/home/tinuvile/NimlothOS/os/target/riscv64gc-unknown-none-elf/debug/NimlothOS: /home/tinuvile/NimlothOS/os/src/lang_items.rs /home/tinuvile/NimlothOS/os/src/main.rs
---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/NimlothOS-80bd4d519888317c.d
@@ -0,0 +1,6 @@
+/home/tinuvile/NimlothOS/os/target/riscv64gc-unknown-none-elf/debug/deps/NimlothOS-80bd4d519888317c.d: src/main.rs src/lang_items.rs
+
+/home/tinuvile/NimlothOS/os/target/riscv64gc-unknown-none-elf/debug/deps/NimlothOS-80bd4d519888317c: src/main.rs src/lang_items.rs
+
+src/main.rs:
+src/lang_items.rs:
