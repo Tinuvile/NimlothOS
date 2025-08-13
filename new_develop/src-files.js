20baa45aa1adac8d68ab68d41013db9c9bdc7959
Tinuvile <Tinuvile@users.noreply.github.com>
2025-08-13 13:20:39 +0000

    deploy: a0419fc00e1762b38a5e864560c71de3cf80c9ae

--- new_develop/src-files.js
+++ new_develop/src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["os",["",[],["console.rs","lang_items.rs","main.rs","sbi.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["os",["",[],["console.rs","lang_items.rs","log.rs","main.rs","sbi.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[64]}
+//{"start":36,"fragment_lengths":[73]}
