2bc0b7ad2cc74d7450d7e8b960debd9ee33eba4c
Tinuvile <Tinuvile@users.noreply.github.com>
2025-08-13 03:01:06 +0000

    deploy: 326356c396f90da308a02f08cf925ff4e77ad166

---
+++ new_develop/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = new Map(JSON.parse('[["os",{"t":"CH","n":["lang_items","panic"],"q":[[0,"os"],[1,"os::lang_items"],[2,"core::panic::panic_info"]],"i":"``","f":"`{{{d{b}}}f}","D":"`","p":[[5,"PanicInfo",2],[1,"reference",null,null,1],[1,"never"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAIAEAAAAAAAAQACAA==","P":[]}]]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
+//{"start":39,"fragment_lengths":[289]}
---
+++ new_develop/search.desc/os/os-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("os", 0, "")
