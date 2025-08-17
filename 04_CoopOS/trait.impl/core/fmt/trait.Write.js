9b989bb83837911f0feee517075e1cc1309858f0
Tinuvile <Tinuvile@users.noreply.github.com>
2025-08-17 13:41:38 +0000

    deploy: 1ee93833b759e6ed7a02dce3dff75803f48047cc

---
+++ 04_CoopOS/trait.impl/core/fmt/trait.Write.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl Write for <a class=\"struct\" href=\"os/console/struct.Stdout.html\" title=\"struct os::console::Stdout\">Stdout</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[134]}
---
+++ 04_CoopOS/trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl Copy for <a class=\"enum\" href=\"os/task/task/enum.TaskStatus.html\" title=\"enum os::task::task::TaskStatus\">TaskStatus</a>"],["impl Copy for <a class=\"struct\" href=\"os/loader/struct.KernelStack.html\" title=\"struct os::loader::KernelStack\">KernelStack</a>"],["impl Copy for <a class=\"struct\" href=\"os/loader/struct.UserStack.html\" title=\"struct os::loader::UserStack\">UserStack</a>"],["impl Copy for <a class=\"struct\" href=\"os/sbi/struct.SbiRet.html\" title=\"struct os::sbi::SbiRet\">SbiRet</a>"],["impl Copy for <a class=\"struct\" href=\"os/task/context/struct.TaskContext.html\" title=\"struct os::task::context::TaskContext\">TaskContext</a>"],["impl Copy for <a class=\"struct\" href=\"os/task/task/struct.TaskControlBlock.html\" title=\"struct os::task::task::TaskControlBlock\">TaskControlBlock</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[842]}
