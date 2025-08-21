06ca3d0cf39090809d6fca73eceeeb44d80be06f
Tinuvile <Tinuvile@users.noreply.github.com>
2025-08-21 13:39:27 +0000

    deploy: bd960e50cae07e38357a9ec7faae91a84c0b8ee0

---
+++ 07_ProcessOS/os/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["clear_bss","rust_main"],"macro":["debug","error","info","print","println","trace","warn"],"mod":["board","config","console","lang_items","loader","log","mm","sbi","stack_trace","sync","syscall","task","timer","trap"]};
---
+++ 07_ProcessOS/os/task/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["add_initproc","exit_current_and_run_next","suspend_current_and_run_next"],"mod":["context","manager","pid","processor","switch","task"],"struct":["INITPROC"]};
